edc73bd3be33018afe296e3a5ff81dd3c7967ea9
SimHub <lackmann.simon@gmail.com>
2019-10-03 00:25:39 +0200

    modified app

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ function stopStreamedVideo(videoElem) {
 }
 video.addEventListener('play', () => {
   //status.innerHTML = '<code class="label">initialize  detection..</code>';
-  statusCode.innerHTML = 'initialize  detection..';
+  statusCode.innerHTML = 'initialize detection';
   // avatarLamp.style.backgroundColor = 'orange';
   panel.style.height = '400px';
   avatarImgStart.style.display = 'block';
@@ -108,6 +108,8 @@ video.addEventListener('play', () => {
     if (detections[0]) {
       gender = detections[0].gender;
       // status.innerHTML= "";
+      status.style.visibility = "hidden";
+      status.style.display = "none";
       //statusCode.style.dislay= "none";
       statusCode.innerHTML= "";
       loader.style.display = "none"; // hide preloader
